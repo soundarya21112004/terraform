494fd06cbc093ca4578f64e8eca57810baf8cef4
soundarya21112004 <soundarya21112004@gmail.com>
2026-05-08 10:41:17 +0530

    tf script for creating the ec2

---
+++ modules/ec2_instance/main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = { Name = "Terraform-Module-EC2" }
+}
---
+++ modules/ec2_instance/variable.tf
@@ -0,0 +1,3 @@
+variable "ami_id" { default = "ami-007020fd9c84e18c7" } # Ubuntu 22.04 in Mumbai
+variable "instance_type" { default = "t3.medium" }
+variable "key_name" { type = string }
